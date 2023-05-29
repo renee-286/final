13d91d84523eb3a1c0c6a7dd6863023a6b5594af
renee-286 <133666195+renee-286@users.noreply.github.com>
2023-05-29 14:04:51 +0530

    Uploaded Final Assignment
    Final assignment done on Jupyter Notebook downloaded and uploaded here

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# __Objectives:__
+#     
+#     - Listing popular data science languages
+#     - Listing common data science libraries
+#     - Creating a table of open source data science tools
+#     - Writing simple arithmetic expressions in python
+
+# Some popular languages that data scientists use are:
+# 
+#     1. Python    
+#     2. R    
+#     3. SQL
+
+# Some libraries that are commonly use in data science are:
+#     
+#     1. Pandas
+#     2. NumPy
+#     3. ggplot2
+
+# 
+# |Data Science Tools  |
+# |--------------------|
+# |TensorFlow          |
+# |RStudio             |
+# |Apache Spark        |
+# 
+
+# ### Examples of arithmetic expressions in Python:
+# 
+
+# In[1]:
+
+
+(3*4)+5
+#Example of a simple arithmetic expression
+
+
+# In[4]:
+
+
+#Converting 200 minutes to hours
+a= 200/60
+print('200 minutes equal', a, 'hours')
+
+
+# ## Author
+# Aparajita
+
+# In[ ]:
+
+
+
+
